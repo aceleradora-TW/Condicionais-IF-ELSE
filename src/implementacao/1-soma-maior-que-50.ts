313382f044586f8939cd287fbccb4d5491951b9a
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-12 11:16:16 -0300

    Questão 1

--- src/implementacao/1-soma-maior-que-50.ts
+++ src/implementacao/1-soma-maior-que-50.ts
@@ -1,3 +1,12 @@
 function somaMaiorQueCinquenta() {
-    return null; 
+    let valor1 = 10;
+    let valor2 = 8;
+    let soma = valor1 + valor2;
+
+    if(soma > 50){
+        return "O resultado da soma é: " + soma;
+    }
+    else{
+        return "O resultado da soma é menor que 50"
+    }
 }
