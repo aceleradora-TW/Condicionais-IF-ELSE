cd4f71451aafe55643e3b891585ee6db9c057df5
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-12 13:29:48 -0300

    adição de casa decimal

--- src/implementacao/2-bonificacao-vendedor.ts
+++ src/implementacao/2-bonificacao-vendedor.ts
@@ -3,7 +3,7 @@ function calcularBonificacao(valorUnit: number, valorVendido: number) {
     let soma = valorVendido * valorUnit;
 
     if ( soma > 100) {
-        return  "Você vendeu: " + soma + ". Bonificação de 10% recebida!"
+        return  "Você vendeu: " + soma.toFixed(2) + ". Bonificação de 10% recebida!" //.toFixed(2) formata o número com duas casas decimais
     }
     else {
         return "Vendedor não atingiu o valor mínimo para recebimento de bonificação."
@@ -11,4 +11,4 @@ function calcularBonificacao(valorUnit: number, valorVendido: number) {
 
 }
 
-console.log(calcularBonificacao(10, 120)); 
+console.log(calcularBonificacao(10.30, 120.20)); 
