4bf1d3286c5cdf3732a3dcf658c12a20e51e3723
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-12 12:07:38 -0300

    Resolução bonificação vendedor

--- src/implementacao/2-bonificacao-vendedor.ts
+++ src/implementacao/2-bonificacao-vendedor.ts
@@ -1,4 +1,14 @@
-function calcularBonificacao() {
-    return null;
+function calcularBonificacao(valorUnit: number, valorVendido: number) {
+
+    let soma = valorVendido * valorUnit;
+
+    if ( soma > 100) {
+        return  "Você vendeu: " + soma + ". Bonificação de 10% recebida!"
+    }
+    else {
+        return "Vendedor não atingiu o valor mínimo para recebimento de bonificação."
+    }
+
 }
 
+console.log(calcularBonificacao(10, 120)); 
