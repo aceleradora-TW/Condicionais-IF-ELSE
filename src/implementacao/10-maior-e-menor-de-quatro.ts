65fe94a2994de17bd8e40ca75c23e3129500bd6e
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-13 14:25:24 -0300

    Finalizando a lista de exercícios

--- src/implementacao/10-maior-e-menor-de-quatro.ts
+++ src/implementacao/10-maior-e-menor-de-quatro.ts
@@ -1,36 +1,32 @@
-//Construa um algoritmo que, dado quatro valores, A, B, C e D, o algoritmo diga qual é o maior e o menor valor.
-
 function determinarMaiorMenorDeQuatro(A: number, B: number, C: number, D: number) {
+    let MaiorNumero: number;
+    let MenorNumero: number;
 
-    let MaiorNumero;
-    if(MaiorNumero = A > B && A > C && A > D){
-        return("O maior número é:" + A)
-    }
-    else if(MaiorNumero = B > A && B > C && B > D){
-        return("O maior número é:" + B)
-    }
-    else if(MaiorNumero = C > A && C > B && C > D){
-        return("O maior número é:" + C)
-    }
-    else{MaiorNumero = D
-        return("O maior número é:" + D)
-    }
-
-    let MenorNumero;
-    if(MenorNumero = A < B && A < C && A < D){
-        return("E o menor número é:" + A)
-    }
-    else if(MenorNumero = B < A && B < C && B < D){
-        return("E o menor número é:" + B)
-    }
-    else if(MenorNumero = C < A && C < B && C < D){
-        return("E o menor número é:" + C)
-    }
-    else{MenorNumero = D
-        return("E o menor número é:" + D)
+    // Determinar o maior número
+    if (A > B && A > C && A > D) {
+        MaiorNumero = A;
+    } else if (B > A && B > C && B > D) {
+        MaiorNumero = B;
+    } else if (C > A && C > B && C > D) {
+        MaiorNumero = C;
+    } else {
+        MaiorNumero = D;
     }
 
-    return MaiorNumero, MenorNumero;
+    // Determinar o menor número
+    if (A < B && A < C && A < D) {
+        MenorNumero = A;
+    } else if (B < A && B < C && B < D) {
+        MenorNumero = B;
+    } else if (C < A && C < B && C < D) {
+        MenorNumero = C;
+    } else {
+        MenorNumero = D;
+    }
+
+    console.log("O maior número é: " + MaiorNumero);
+    console.log("E o menor número é: " + MenorNumero);
 }
 
-determinarMaiorMenorDeQuatro(4.10,20,50)
+// Exemplo de uso
+determinarMaiorMenorDeQuatro(4, 10, 20, 50);
--- src/implementacao/13-emprestimo-salario.ts
+++ src/implementacao/13-emprestimo-salario.ts
@@ -1,3 +1,9 @@
-function verificarEmprestimo() {
-    return null;
+function verificarEmprestimo(salarioBruto: number, valorPrestacao: number) {
+    let valorEmprestimo = salarioBruto * (0.30);
+    if(valorPrestacao > valorEmprestimo){
+        return `O valor ${valorPrestacao} ultrapassa o limite de 30% do salário bruto que é ${valorEmprestimo}`;
+    }else{
+        return `Empréstimo concedido com sucesso, valor da prestação é ${valorPrestacao}!!`
+    }
 }
+console.log(verificarEmprestimo(1200,300));
